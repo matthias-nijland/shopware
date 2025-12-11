c43adb011c8ab7c006f0be3e302367dc3a1414dc
Sebastian Franze <s.franze@shopware.com>
2025-12-11 16:26:58 +0100

    feat(product-analytics): Load consents in Admin

--- src/Administration/Resources/app/administration/src/global.types.ts
+++ src/Administration/Resources/app/administration/src/global.types.ts
@@ -143,7 +143,7 @@ import type CMSConstant from './module/sw-cms/constant/sw-cms.constant';
 import type CUSTOMERConstant from './module/sw-customer/constant/sw-customer.constant';
 import type FLOWConstant from './module/sw-flow/constant/flow.constant';
 import type SnippetApiService from './core/service/api/snippet.api.service';
-
+import type ConsentApiService from './core/consent/consent.api.service';
 // trick to make it an "external module" to support global type extension
 
 // base methods for subContainer
@@ -297,6 +297,7 @@ declare global {
         ssoSettingsService: SsoSettingsService;
         ssoInvitationService: SsoInvitationService;
         shopIdChangeService: ShopIdChangeService;
+        consentApiService: ConsentApiService;
     }
 
     interface MixinContainer {
